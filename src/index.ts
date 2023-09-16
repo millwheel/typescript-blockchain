6f71fa0c7e1820396a3f946e696a9411854cfe8d
season <sionwer4@naver.com>
2023-09-16 22:24:19 +0900

    Create blockchain

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,29 @@ class Block implements BlockShape{
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain{
+    private blocks: Block[];
+    constructor(){
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data:string){
+        const block = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(block);
+    }
+    public getBlocks(){
+        return this.blocks;
+    }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock("First");
+blockchain.addBlock("Second");
+blockchain.addBlock("Third");
+
+console.log(blockchain.getBlocks());
+

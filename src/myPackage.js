b2ec084969126db49b93bb6deedbc2a3ce022f96
season <sionwer4@naver.com>
2023-09-16 22:15:42 +0900

    Create block

---
+++ src/myPackage.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {boolean} config.url 
+ * @returns boolean
+ */
+
+export function init(config){
+    return true;
+}
+/**
+ * 
+ * @param {number} code 
+ * @returns number
+ */
+export function exit(code){
+    return code + 1;
+}
